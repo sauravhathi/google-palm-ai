5fca0edb0c9e0c05f7c0a428fe4f88a0c61a762f
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-11-04 02:13:35 +0530

    Update accessKeyModel.ts

--- pages/api/db/models/accessKeyModel.ts
+++ pages/api/db/models/accessKeyModel.ts
@@ -1,41 +1,42 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const accessKeySchema = new mongoose.Schema({
-    accessKey: {
-        type: String,
-        unique: true,
+  accessKey: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  totalRequests: {
+    type: Number,
+    default: 0,
+  },
+  maxRequests: {
+    type: Number,
+    default: process.env.MAX_REQUESTS_API_KEY || 15,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expiresAt: {
+    type: Date,
+    default: () => {
+      const expiresAt = new Date();
+      expiresAt.setDate(expiresAt.getDate() + 3);
+      return expiresAt;
     },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    totalRequests: {
-        type: Number,
-        default: 0,
-    },
-    maxRequests: {
-        type: Number,
-        default: 100,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now,
-    },
-    expiresAt: {
-        type: Date,
-        default: () => {
-            const expiresAt = new Date();
-            expiresAt.setDate(expiresAt.getDate() + 3);
-            return expiresAt;
-        }
-    },
+  },
 });
 
-const AccessKey = mongoose.models.AccessKey || mongoose.model('AccessKey', accessKeySchema);
+const AccessKey =
+  mongoose.models.AccessKey || mongoose.model("AccessKey", accessKeySchema);
 
 export default AccessKey;
